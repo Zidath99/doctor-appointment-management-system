550b4ff4d71bb5eabfc2657b204d7ac6c7007796
Sidath <bmsnbalasooriya@students.nsbm.ac.lk>
2021-06-19 22:55:10 +0530

    First Commit

---
+++ Databse/DatabaseConnection.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+
+namespace Doctor_Appointment_Management_System.Databse
+{
+    class DatabaseConnection
+    {
+        private static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='C:\Users\Nipun\source\repos\Doctor Appointment Management System\db.mdf';Integrated Security=True";
+        private static SqlConnection connection;
+        public static SqlConnection getConnection() {
+            connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
---
+++ User/Login.cs
@@ -0,0 +1,48 @@
+﻿using Doctor_Appointment_Management_System.Main;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Doctor_Appointment_Management_System.User
+{
+    public partial class Login : Form
+    {
+        private SqlConnection databaseConnection;
+        public Login()
+        {
+            InitializeComponent();
+            this.databaseConnection = Databse.DatabaseConnection.getConnection();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            Hide(); // hide login form
+
+            // create new instance of Admin View 
+            AdminView adminView = new AdminView();
+            adminView.Show(); // show admin view
+        }
+    }
+}
