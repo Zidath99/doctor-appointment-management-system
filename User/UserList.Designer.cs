b5da2e0e80ea09fb877a7cd77307a40d26421ab0
Sidath <bmsnbalasooriya@students.nsbm.ac.lk>
2021-06-20 17:29:14 +0530

    User - List User - Load users table

--- User/UserList.Designer.cs
+++ User/UserList.Designer.cs
@@ -29,19 +29,53 @@ namespace Doctor_Appointment_Management_System.User
         /// </summary>
         private void InitializeComponent()
         {
+            this.tblUserList = new System.Windows.Forms.DataGridView();
+            this.btnAddUser = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tblUserList)).BeginInit();
             this.SuspendLayout();
+            // 
+            // tblUserList
+            // 
+            this.tblUserList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tblUserList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.tblUserList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tblUserList.Location = new System.Drawing.Point(12, 41);
+            this.tblUserList.Name = "tblUserList";
+            this.tblUserList.ReadOnly = true;
+            this.tblUserList.RowHeadersWidth = 51;
+            this.tblUserList.RowTemplate.Height = 24;
+            this.tblUserList.Size = new System.Drawing.Size(955, 535);
+            this.tblUserList.TabIndex = 0;
+            // 
+            // btnAddUser
+            // 
+            this.btnAddUser.Location = new System.Drawing.Point(12, 12);
+            this.btnAddUser.Name = "btnAddUser";
+            this.btnAddUser.Size = new System.Drawing.Size(75, 23);
+            this.btnAddUser.TabIndex = 1;
+            this.btnAddUser.Text = "Add";
+            this.btnAddUser.UseVisualStyleBackColor = true;
+            this.btnAddUser.Click += new System.EventHandler(this.btnAddUser_Click);
             // 
             // UserList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(747, 450);
+            this.ClientSize = new System.Drawing.Size(979, 588);
+            this.Controls.Add(this.btnAddUser);
+            this.Controls.Add(this.tblUserList);
             this.Name = "UserList";
             this.Text = "UserList";
+            ((System.ComponentModel.ISupportInitialize)(this.tblUserList)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView tblUserList;
+        private System.Windows.Forms.Button btnAddUser;
     }
 }
--- User/UserList.cs
+++ User/UserList.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,37 @@ namespace Doctor_Appointment_Management_System.User
 {
     public partial class UserList : Form
     {
+        private SqlConnection databaseConnection;
         public UserList()
         {
             InitializeComponent();
+            this.databaseConnection = Databse.DatabaseConnection.getConnection();
         }
+
+        private void loadUsers()
+        {
+
+            // open database connection
+            Databse.DatabaseConnection.open();
+
+            // creare Datatable object
+            DataTable userListDataTable = new DataTable();
+            SqlDataAdapter auserListSqlAdapter = new SqlDataAdapter("select username as Username, name as Name, email as Email, user_type as [User Type] from [user]", this.databaseConnection);
+
+            auserListSqlAdapter.Fill(userListDataTable);
+            tblUserList.DataSource = userListDataTable;
+
+            //close database connection
+            Databse.DatabaseConnection.close();
+
+        }
+
+        private void btnAddUser_Click(object sender, EventArgs e)
+        {
+            UserForm userForm = new UserForm();
+            userForm.Show();
+        }
+
+
     }
 }
