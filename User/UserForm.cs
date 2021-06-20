07373b61b466069663543f76417c30d7173b7416
Sidath <bmsnbalasooriya@students.nsbm.ac.lk>
2021-06-20 13:01:22 +0530

    User - create user - insert user into database

--- User/UserForm.cs
+++ User/UserForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,10 +20,33 @@ namespace Doctor_Appointment_Management_System.User
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            String username = txtUsername.Text;
-            String name = txtName.Text;
-            String email = txtEmail.Text;
-            String password = txtPassword.Text;
+            try
+            {
+                // create databse connection
+                SqlConnection databaseConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Nipun\source\repos\doctor-appointment-management-system\db.mdf;Integrated Security=True");
+                SqlCommand userInsertCommand;
+
+                userInsertCommand = new SqlCommand("INSERT INTO [user] (username,name,email,password) VALUES (@username,@name,@email,@password)", databaseConnection);
+                databaseConnection.Open(); // open databse
+
+                // bind values to insert query
+                userInsertCommand.Parameters.AddWithValue("@username", txtUsername.Text);
+                userInsertCommand.Parameters.AddWithValue("@name", txtName.Text);
+                userInsertCommand.Parameters.AddWithValue("@email", txtEmail.Text);
+                userInsertCommand.Parameters.AddWithValue("@password", txtPassword.Text);
+
+                // execute the insert command, data will be insert into database
+                userInsertCommand.ExecuteNonQuery();
+
+                // we do not need the connection any more, close the database connection
+                databaseConnection.Close();
+
+                // show success message to user
+                MessageBox.Show("User saved successfully!");
+            }
+            catch (Exception ex) {
+                MessageBox.Show("User save failed: "+ex.Message);
+            }
         }
 
        
