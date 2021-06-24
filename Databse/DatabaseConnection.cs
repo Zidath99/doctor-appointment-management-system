7f74348bb9ca3ee66e373eb5f3fd3113a236fefb
Zidath99 <bmsnbalasooriya@nsbm.ac.lk>
2021-06-24 12:16:56 +0530

    New database added with all tables

--- Databse/DatabaseConnection.cs
+++ Databse/DatabaseConnection.cs
@@ -5,7 +5,7 @@ namespace Doctor_Appointment_Management_System.Databse
 {
     class DatabaseConnection
     {
-        private static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='C:\Users\Nipun\source\repos\Doctor Appointment Management System\db.mdf';Integrated Security=True";
+        private static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Nipun\doctor-appointment-management-system\database.mdf;Integrated Security=True";
         private static SqlConnection connection;
         public static SqlConnection getConnection() {
             connection = new SqlConnection(connectionString);
